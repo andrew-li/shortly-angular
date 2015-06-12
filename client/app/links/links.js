973a67407ffd8ac91728881758c4b1d85acc24d9
andrew-li <ali91007@gmail.com>
2015-06-12 15:35:43 -0700

    Configure $scope.data and $scope.getLinks() for LinksController

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,14 @@
 angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
-  // Your code here
+
+  $scope.data = {};
+  $scope.getLinks = function($http) {
+    Links.retrieveLinks().success(function (data) {
+      $scope.data.links = data;
+    });
+  };
+
+  $scope.getLinks();
+
 });
