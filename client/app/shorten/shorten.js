808cc408052ca779a29c94e4714f4a4124d47c54
andrew-li <ali91007@gmail.com>
2015-06-12 17:41:19 -0700

    Modified $scope.addLink()

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,7 +2,7 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
   $scope.link = {};
-  $scope.addLink = function() {
-    Links.sendLink($location).success(function() {});
+  $scope.addLink = function(loc) {
+    Links.sendLink(loc).success(function() {});
   };
 });
